464e210ad766aa2e116f38bd53f07ea4a2c6baaf
Gaurav Singh <singh.gaurav.cs@gmail.com>
2023-08-26 17:09:40 +0530

    added an html file and icon

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const { app, BrowserWindow}  = require ('electron')
 
 function createMainWindow () {
     const mainWindow = new BrowserWindow({
-        title: 'IMageShrink',
+        title: 'ImageShrink',
+        icon: '/assessts/icons/icon-camera',
         width: 500,
         height: 600
     })
+    console.log(__dirname)
+   // mainWindow.loadURL(`file:/${__dirname}/app/index.html`)
+    mainWindow.loadFile(`./app/index.html`)
+
 }
 
 app.on('ready', createMainWindow)
